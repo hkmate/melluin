c688be2b4df222f11e9fbcddae3e853161ee7cfe
Máté Herget <mail.kmate@gmail.com>
2022-12-31 22:50:29 +0100

    feat(fe): Add placeholder text and icon to dashboard.
    NM-45

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -2,11 +2,16 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 import {DashboardComponent} from './dashboard.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     imports: [
         BrowserModule,
         RouterModule,
+
+        TranslateModule,
+        MatIconModule,
     ],
     declarations: [DashboardComponent]
 })
