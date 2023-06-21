5603ce1c7b20fbcfca074769712fa182abed8d7b
Máté Herget <mail.kmate@gmail.com>
2023-06-22 00:37:38 +0200

    chore: Add Dockerfiles to both backend and frontend
    NM-120

--- dev-env/set-frontend-config.js
+++ dev-env/set-frontend-config.js
@@ -4,7 +4,7 @@ const CONFIG_FILE = 'frontend/src/assets/app-config.json';
 let rawData = fs.readFileSync(CONFIG_FILE);
 let config = JSON.parse(rawData);
 
-console.log(`set baseURL: ${process.env.API_URL}`);
+console.log(`Set base url: ${process.env.API_URL}`);
 config.baseURL = process.env.API_URL;
 
 let data = JSON.stringify(config, null, '    ');
