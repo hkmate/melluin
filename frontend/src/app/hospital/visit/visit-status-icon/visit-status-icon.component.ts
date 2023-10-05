b29444a18d5e940c1632abcfa653c77563d523a3
Máté Herget <mail.kmate@gmail.com>
2023-10-06 00:26:30 +0200

    feat: Add few new hospital visit status and rename few others.
    We have to add new statuses that contains information why the visit failed.
    Also needed to rename few, because in the last discussion with the coordinator we decided that the personal reports (coming soon) will be mandatory so the old status names weren't accurate anymore.
    
    NM-141

--- frontend/src/app/hospital/visit/visit-status-icon/visit-status-icon.component.ts
+++ frontend/src/app/hospital/visit/visit-status-icon/visit-status-icon.component.ts
@@ -17,7 +17,28 @@ import {MatIconModule} from '@angular/material/icon';
 })
 export class VisitStatusIconComponent {
 
+    private static readonly STATUS_ICON_MAPPER = {
+        [HospitalVisitStatus.DRAFT]: 'draw',
+        [HospitalVisitStatus.SCHEDULED]: 'calendar_month',
+
+        [HospitalVisitStatus.STARTED]: 'theater_comedy',
+
+        [HospitalVisitStatus.ACTIVITIES_FILLED_OUT]: 'fact_check',
+        [HospitalVisitStatus.ALL_FILLED_OUT]: 'inventory_2',
+
+        [HospitalVisitStatus.CANCELED]: 'event_busy',
+        [HospitalVisitStatus.FAILED_BECAUSE_NO_CHILD]: 'face_retouching_off',
+        [HospitalVisitStatus.FAILED_FOR_OTHER_REASON]: 'do_not_touch',
+        [HospitalVisitStatus.SUCCESSFUL]: 'check_circle',
+    }
+
+    protected icon: string;
+    protected visitStatus: HospitalVisitStatus;
+
     @Input()
-    public status: HospitalVisitStatus;
+    public set status(value: HospitalVisitStatus) {
+        this.visitStatus = value;
+        this.icon = VisitStatusIconComponent.STATUS_ICON_MAPPER[value];
+    }
 
 }
