edfe1e82c523649ddd9911890681603f7dd52f77
Máté Herget <mail.kmate@gmail.com>
2023-11-12 18:03:44 +0100

    build: Update packages, major change is Angular 17
    NM-161

--- frontend/src/app/auth/service/jwt.service.ts
+++ frontend/src/app/auth/service/jwt.service.ts
@@ -1,11 +1,11 @@
 import {Injectable} from '@angular/core';
-import jwt_decode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable()
 export class JwtService {
 
     public decodeToken(token: string): string {
-        return jwt_decode(token);
+        return jwtDecode(token);
     }
 
 }
