01043b1ed1b22c56cc9e0cef67d896092c0a08bc
Máté Herget <mail.kmate@gmail.com>
2025-08-24 00:37:01 +0200

    feat(fe): Add 'created' and 'createdBy' to people list and people and details
    NM-194

--- frontend/src/app/people/person-detail/person-data-persenter/person-data-presenter.component.ts
+++ frontend/src/app/people/person-detail/person-data-persenter/person-data-presenter.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input} from '@angular/core';
-import {Person} from '@shared/person/person';
+import { Component, effect, inject, input, signal } from '@angular/core';
+import { Person } from '@shared/person/person';
+import { PeopleService } from '@fe/app/people/people.service';
+import { isNotNil } from '@shared/util/util';
 
 @Component({
     selector: 'app-person-data-presenter',
@@ -8,7 +10,21 @@ import {Person} from '@shared/person/person';
 })
 export class PersonDataPresenterComponent {
 
-    @Input()
-    public person?: Person;
+    private readonly peopleService = inject(PeopleService);
+
+    public person = input<Person>();
+
+    protected creator = signal<Person | undefined>(undefined);
+
+    constructor() {
+        effect(() => {
+            const creatorId = this.person()?.createdByPersonId;
+            if (isNotNil(creatorId)) {
+                this.peopleService.getPerson(creatorId).subscribe(person => {
+                    this.creator.set(person);
+                });
+            }
+        });
+    }
 
 }
