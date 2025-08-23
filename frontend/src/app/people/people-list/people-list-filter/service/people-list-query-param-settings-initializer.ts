f2845f5fd20abc6e9486c21c73a489c9de38c774
Máté Herget <mail.kmate@gmail.com>
2025-08-24 00:09:41 +0200

    feat: Set default filter for 'onlyActive' to false in people list

--- frontend/src/app/people/people-list/people-list-filter/service/people-list-query-param-settings-initializer.ts
+++ frontend/src/app/people/people-list/people-list-filter/service/people-list-query-param-settings-initializer.ts
@@ -30,7 +30,7 @@ export class PeopleListQueryParamSettingsInitializer {
         result.name = '';
         result.email = '';
         result.phone = '';
-        result.onlyActive = true;
+        result.onlyActive = false;
         result.roleNames = [];
         return result;
     }
