306ff8f018c67e1ba662e0b52672361db5fae40d
Máté Herget <mail.kmate@gmail.com>
2025-08-24 02:38:48 +0200

    feat(fe): Add 'no server' error message

--- frontend/src/app/util/util.ts
+++ frontend/src/app/util/util.ts
@@ -2,14 +2,17 @@ import {catchError, OperatorFunction, throwError} from 'rxjs';
 import {MessageService} from '@fe/app/util/message.service';
 import {AbstractControl, ValidationErrors} from '@angular/forms';
 import {isNilOrEmpty} from '@shared/util/util';
+import {HttpErrorResponse} from '@angular/common/http';
 
 export function utf8ToBase64(str: string): string {
     return window.btoa(unescape(encodeURIComponent(str)));
 }
 
 export function getErrorHandler<T>(msg: MessageService): OperatorFunction<T, T> {
-    return catchError(error => {
-        if ('message' in error.error) {
+    return catchError((error: HttpErrorResponse) => {
+        if (error.status === 0) {
+            msg.error('NoBackendError');
+        } else if ('message' in error.error) {
             if (error.error.message instanceof Array) {
                 msg.errorRaw((error.error.message as Array<string>).join(','));
             }
